fe7faaa6d287bec8edefcd95d1767ebf51058120
DarioBosco <dario_bosco@protonmail.com>
2021-10-01 11:01:37 +0200

    Aggiunta colori palette in variables.scss

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,4 +5,6 @@
 <script></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../assets/styles/variables';
+</style>
