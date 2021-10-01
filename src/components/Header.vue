57fb234a91938922ccad1103a6cde8a45f604f04
DarioBosco <dario_bosco@protonmail.com>
2021-10-01 10:43:49 +0200

    Aggiunti i componenti principali

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+	<header></header>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,8 @@
+<template>
+	<main></main>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
