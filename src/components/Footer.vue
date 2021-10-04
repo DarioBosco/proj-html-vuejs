2e640a5bdf7ab763b4b50ac0d5517614c263e5e4
DarioBosco <dario_bosco@protonmail.com>
2021-10-04 12:04:00 +0200

    Inizio struttura footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,49 @@
 <template>
-	<footer></footer>
+	<footer>
+		<div class="container">
+			<div class="footer-top">
+				<div class="logo">
+					<img :src="images.logo" alt="" />
+				</div>
+			</div>
+		</div>
+	</footer>
 </template>
 
-<script></script>
+<script>
+export default {
+	data() {
+		return {
+			images: {
+				logo: 'https://avada.theme-fusion.com/bakery/wp-content/uploads/sites/124/2021/04/avada-bakery-logo.svg',
+			},
+			links: {
+				shop: '#',
+				about: '#',
+				gallery: '#',
+				locations: '#',
+				journal: '#',
+				contact: '#',
+				orders: '#',
+				cart: '#',
+				allRightsReserved: '',
+				wordpress: '#',
+				instagram: '#',
+				twitter: '#',
+				facebook: '#',
+				pinterest: '#',
+			},
+		};
+	},
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '../assets/styles/variables';
 @import '../assets/styles/common';
+
+footer {
+	padding-top: 100px;
+}
 </style>
--- src/components/OurProducts.vue
+++ src/components/OurProducts.vue
@@ -6,11 +6,31 @@
 			<button>Start Shopping</button>
 		</div>
 		<div class="images-box">
-			<div class="image-box">
-				Immagine
+			<div class="arrows">
+				<div class="left">
+					<i class="fas fa-chevron-left"></i>
+				</div>
+				<div class="right">
+					<i class="fas fa-chevron-right"></i>
+				</div>
 			</div>
 			<div class="image-box">
-				Immagine
+				<div class="hoverlay">
+					<!-- Bellissimo nome per una classe del genere -->
+					<h2>Choco Chip Cookies</h2>
+					<h6>Cookies, Pastries</h6>
+					<h2>$ 19.00 $39.00</h2>
+				</div>
+				<img src="https://avada.theme-fusion.com/bakery/wp-content/uploads/sites/124/2019/11/choco-chip-cookies.jpg" alt="" />
+			</div>
+			<div class="image-box">
+				<div class="hoverlay">
+					<!-- Bellissimo nome per una classe del genere -->
+					<h2>Strawberry Jam Cookies</h2>
+					<h6>Cookies, Pastries</h6>
+					<h2>$ 19.00 $39.00</h2>
+				</div>
+				<img src="https://avada.theme-fusion.com/bakery/wp-content/uploads/sites/124/2019/11/strawberry-jam-cookies.jpg" alt="" />
 			</div>
 		</div>
 	</div>
@@ -27,8 +47,8 @@ export default {
 @import '../assets/styles/common';
 .container {
 	display: flex;
-	align-items: center;
 	justify-content: center;
+	padding-top: 100px;
 
 	.text-box {
 		width: calc(100% / 3);
@@ -48,6 +68,7 @@ export default {
 			font-family: 'Source Serif Pro';
 			color: $daisyBush;
 			padding-right: 20%;
+			font-size: 2rem;
 		}
 
 		button {
@@ -58,13 +79,58 @@ export default {
 
 	.images-box {
 		width: calc(100% / 3 * 2);
-		height: 100px;
+		height: 720px;
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
+		position: relative;
+
+		.arrows {
+			position: absolute;
+			display: flex;
+			justify-content: space-between;
+			width: 100%;
+
+			& * {
+				background-color: $perfume;
+				height: 80px;
+				width: 40px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				color: white;
+			}
+		}
 		.image-box {
-			background: red;
 			width: calc(50% - 10px);
+			height: 100%;
+			position: relative;
+
+			.hoverlay {
+				position: absolute;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				visibility: hidden;
+				opacity: 0;
+				transition: visibility 0s, opacity 0.5s linear;
+				color: white;
+				background: rgba(53, 49, 49, 0.671);
+				height: 100%;
+				width: 100%;
+			}
+
+			&:hover .hoverlay {
+				visibility: visible;
+				opacity: 1;
+			}
+
+			img {
+				width: 100%;
+				max-height: 100%;
+				object-fit: cover;
+			}
 		}
 	}
 }
